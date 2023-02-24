2bd79004ea159549b3683be77df27b945ef0687e
changjun <52chinaweb@gmail.com>
2023-02-24 11:38:38 +0800

    feat: add head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,9 @@ export default defineNuxtConfig({
         { name: 'description', content: '极简化记录分享我的书单、观影、电影、游戏等' },
       ],
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: `/blog-apple-touch-icon.png` },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/blog-favicon-32x32.png` },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/blog-favicon-16x16.png` },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: `/apple-touch-icon.png` },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: `/favicon-32x32.png` },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: `/favicon-16x16.png` },
       ],
     },
   },
